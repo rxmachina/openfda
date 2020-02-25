5a7ba43051c554f67ac6029fca984ff97302d0d7
rxmachina <60950231+rxmachina@users.noreply.github.com>
2020-02-24 19:53:26 -0500

    Add files via upload

--- faers.R
+++ faers.R
@@ -8,15 +8,15 @@
 #
 # Question:  Are different adverse events reported in different countries?
 #
-# Source data:  2019Q3 data files (REAC19Q3.txt, DEMO19Q3.txt)
+# Source data:  2019Q3 data files (REAC19Q3.txt - 452K entries / 56MB, DEMO19Q3.txt - 1.5M entries / 63MB)
 #
 #     https://fis.fda.gov/content/Exports/faers_ascii_2019Q3.zip
 #
 # Dataset:  for each case, -> join on sorted primaryid, extract DEMO.OCCR_COUNTRY, REAC.PT
 #
-#     % join -t $'\t' -o 2.1,2.25,1.3 REAC19Q3.tab.sorted DEMO19Q3.tab.sorted > REAC+DEMO19Q3.tab.min.sorted &
+#     % /usr/bin/time -l join -t $'\t' -o 2.1,2.25,1.3 REAC19Q3.tab.sorted DEMO19Q3.tab.sorted | gzip -c > REAC+DEMO19Q3.tab.min.sorted.gz &
 #
-#     REF:  https://shapeshed.com/unix-join/
+#     uses ~4.6s real, 850MB max rss
 #
 # Analysis:  normalize country counts by total events -> frequency of each event-type in each country (profile)
 #	     heatmap of countries vs events
@@ -26,7 +26,7 @@
 
 # read in raw data
 #
-events.country <- read.delim("REAC+DEMO19Q3.tab.min.sorted")
+events.country <- read.delim("REAC+DEMO19Q3.tab.min.sorted.gz")
 
 # pivot into count matrix
 #
@@ -60,20 +60,24 @@ ev.heatmap <- heatmap(as.matrix(ev.freq), col= colorRampPalette(brewer.pal(8, "B
 # extract "distinct" event terms for top 8 reporting countries (US, CA, JP, DE, FR, GB, IT, BR)
 #
 ev.top <- ev.freq[ev.heatmap$rowInd,ev.heatmap$colInd[1:8]]
+ev.freq.heat <- ev.freq[ev.heatmap$rowInd,ev.heatmap$colInd]
 
 # determine frequency cutoff for "distinct" terms
 #
-ev.top.vec <- as.vector(as.matrix(ev.top));
-ev.top.hist <- hist(ev.top.vec[ ev.top.vec != 0])  # exclude zero counts
-plot(ev.top.hist, main="Drug Event Frequency Histogram", xlab="Event frequency within a country", ylab="Counts", col="skyblue")
+ev.vec <- as.vector(as.matrix(ev.freq))
+ev.hist <- hist(ev.vec[ ev.vec != 0], main="Drug Event Frequency Histogram", xlab="Event frequency for each country", ylab="Counts", col="skyblue")  # exclude zero counts
+#ev.top.vec <- as.vector(as.matrix(ev.top))
+#ev.top.hist <- hist(ev.top.vec[ ev.top.vec != 0], main="Drug Event Frequency Histogram", xlab="Event frequency for each country", ylab="Counts", col="skyblue")  # exclude zero counts
 
 # filter list of events with freq > cutoff
 #
 cutfreq <- 0.9
 lines(c(cutfreq,cutfreq),c(0,10000),col="red",lty=2,lwd=2)  # show cutoff freq (dashed red line)
-ev.distinct <- ev.top[apply(ev.top, 1, function(x){ any(x > cutfreq); }),]
+ev.distinct <- ev.freq.heat[apply(ev.freq.heat, 1, function(x){ any(x > cutfreq); }),]
+#ev.top.distinct <- ev.top[apply(ev.top, 1, function(x){ any(x > cutfreq); }),]
 
-write.table(ev.distinct, file="events-distinct-top8.txt", sep="\t");
+write.table(ev.distinct, file="events-distinct.txt", sep="\t")
+#write.table(ev.top.distinct, file="events-distinct-top8.txt", sep="\t")
 
 # NOTE:  better approaches could be to use:
 #
